851f1610e0511b77a9cd8ae822f96c26d0eeee12
lucasveigamattos <92687474+lucasveigamattos@users.noreply.github.com>
2023-01-12 10:25:45 -0300

    Delete get-current-tab.js

--- get-current-tab.js
+++
@@ -1,7 +0,0 @@
-async function getCurrentTab() {
-    const queryOptions = {active: true, lastFocusedWindow: true}
-    const [tab] = await chrome.tabs.query(queryOptions)
-    return tab
-}
-
-export default getCurrentTab
