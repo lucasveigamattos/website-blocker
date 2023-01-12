81691a775512f8d00b2733ca3b2b490c88ac8d78
lucasveigamattos <92687474+lucasveigamattos@users.noreply.github.com>
2023-01-12 10:25:55 -0300

    Delete get-local-storage-variable.js

--- get-local-storage-variable.js
+++
@@ -1,8 +0,0 @@
-function getLocalStorageVariable(variableName) {
-    return chrome.storage.local.get([variableName])
-        .then(promiseResult => {
-            return promiseResult[variableName]
-        })
-}
-
-export default getLocalStorageVariable
