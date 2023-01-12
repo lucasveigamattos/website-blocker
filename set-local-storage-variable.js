f7721c76ba066982078689fa119a7794d4dec4e0
lucasveigamattos <92687474+lucasveigamattos@users.noreply.github.com>
2023-01-12 10:26:34 -0300

    Delete set-local-storage-variable.js

--- set-local-storage-variable.js
+++
@@ -1,7 +0,0 @@
-function setLocalStorageVariable(variableName, variableValue) {
-    const valueToBeSaved = {}
-    valueToBeSaved[variableName] = variableValue // ejects the variableName value as a key into the object, and the variableValue as the value
-    chrome.storage.local.set(valueToBeSaved)
-}
-
-export default setLocalStorageVariable
